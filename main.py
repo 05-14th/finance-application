09ec237101a19dd92f10e1157715e5eadfe44100
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2023-11-24 09:36:07 -0500

    Major Additions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys, time
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QPushButton, QWidget, QSizePolicy, QLineEdit
-from PyQt5.QtCore import Qt  # Import Qt module
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QPushButton, QWidget, QSizePolicy, QLineEdit, QFrame, QHBoxLayout, QComboBox, QDateEdit
+from PyQt5.QtCore import Qt, QDate  # Import Qt module
 
 #Initialize Global Variables
 window = None
@@ -140,10 +140,91 @@ class SignupWindow(QWidget):
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle('BudgetBuddy: Expense Tracker App')
+        main_layout = QHBoxLayout()
 
-        main_layout = QVBoxLayout()
+        #Button Sets
+        button_frame = QFrame()
+        button_frame.setFrameShape(QFrame.StyledPanel)
+
+        add_button = QPushButton("Add Expense", self)
+        add_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        view_button = QPushButton("Show Record", self)
+        view_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        remove_button = QPushButton("Remove Expense", self)
+        remove_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        exit_button = QPushButton("Exit", self)
+        exit_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        exit_button.clicked.connect(self.exit_)
+
+        button_layout = QVBoxLayout()
+        button_layout.addWidget(add_button)
+        button_layout.addWidget(view_button)
+        button_layout.addWidget(remove_button)
+        button_layout.addWidget(exit_button)
+        button_frame.setLayout(button_layout)
+
+        #Viewing Area
+        viewing_frame = QFrame()
+        viewing_frame.setFrameShape(QFrame.StyledPanel)
+
+        #Adding View
+        adding_view = QVBoxLayout()
+
+        category_selection = QComboBox()
+        category_selection.addItem('Select a Category')
+        category_selection.addItem('Home & Utilities')
+        category_selection.addItem('Insurance & Financial Section')
+        category_selection.addItem('Obligations')
+        category_selection.addItem('Groceries')
+        category_selection.addItem('Personal & Medical Expense')
+        category_selection.addItem('Entertainment')
+        category_selection.addItem('Transportation Fare')
+        category_selection.addItem('Child Care')
+        category_selection.addItem('Other Expense')
+        category_selection.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        adding_view.addWidget(category_selection)
+
+        item_edit = QLineEdit()
+        item_edit.setPlaceholderText('Description')
+        item_edit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        adding_view.addWidget(item_edit)
+
+        date_edit = QDateEdit()
+        date_edit.setCalendarPopup(True)
+        date_edit.setDate(QDate.currentDate())  # Set default date to current day
+        date_edit.setDisplayFormat("yyyy-MM-dd")  # Set the display format of the date
+        adding_view.addWidget(date_edit)
+
+        amount_edit = QLineEdit()
+        amount_edit.setPlaceholderText('Amount')
+        amount_edit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        adding_view.addWidget(amount_edit)
+
+        payment_method = QComboBox()
+        payment_method.addItem('Payment Method')
+        payment_method.addItem('Cash')
+        payment_method.addItem('Credit')
+        payment_method.addItem('Debit')
+        payment_method.addItem('Gcash')
+        payment_method.addItem('Paymaya')
+        payment_method.addItem('Other')
+        payment_method.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        adding_view.addWidget(payment_method)
+
+        specify_pm = QLineEdit()
+        specify_pm.setPlaceholderText("Specify Payment Method")
+        specify_pm.setEnabled(False)
+        specify_pm.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        adding_view.addWidget(specify_pm)
+
+        #Records Preview
+
+
+
+        viewing_frame.setLayout(adding_view) #Default Frame
+        main_layout.addWidget(button_frame)
+        main_layout.addWidget(viewing_frame)
 
         self.setLayout(main_layout)
 
@@ -151,11 +232,14 @@ class MainWindow(QWidget):
         # Adjust widget sizes or positions upon window resize (if needed)
         pass
 
+    def exit_(self):
+        exit()
+
 
 def main():
     global window
     app = QApplication(sys.argv)
-    window = MyWindow()
+    window = MainWindow()
     window.show()
     app.exec_()
 
